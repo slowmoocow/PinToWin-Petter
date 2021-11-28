50a91dd1cfb3ebd7b566d0ae366b542a94116bd3
Petter Tveit <78284025+slowmoocow@users.noreply.github.com>
2021-11-28 14:13:59 +0100

    Create load.php

---
+++ code_snippets/load.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+<?php
+$temp1 = file_get_contents("data.txt");
+print "<p>$temp1</p>"
+?>
+
+<style type ="text/css">
+@import url('http://fonts.cdnfonts.com/css/common-pixel');
+
+p{
+	text-align:center;
+	font-family: 'Common Pixel', sans-serif;
+	font-size:400px;
+	margin-top:10px;
+	margin-bottom:10px;
+	color: white;
+}
+
+</html>
