f77596cfc4f7103e3f52f9cb242f484eb1a0cb8f
Petter Tveit <78284025+slowmoocow@users.noreply.github.com>
2021-11-28 14:11:50 +0100

    Update pintowin.php

--- code_snippets/pintowin.php
+++ code_snippets/pintowin.php
@@ -1,1 +1,82 @@
-yo
+<!DOCTYPE html>
+
+
+<html>
+<link href="http://fonts.cdnfonts.com/css/common-pixel" rel="stylesheet">
+<head>
+	<title>PinToWin</title>
+</head>
+
+<body>
+	<header>
+		<h1>PinToWin</h1>
+		<form action="/tempApp.php">
+			<div><button class="button button1">Points</button></div><br>
+		</form>
+		<form action="/leaderboard.php">
+			<div><button class="button button2">Leaderboard</button></div>
+		</form>
+	
+<style type ="text/css">
+@import url('http://fonts.cdnfonts.com/css/common-pixel');
+
+body{
+	background-color:black;
+}
+
+header h1{
+	color: #EE82EE;
+	text-align: center;
+	font-family: 'Common Pixel', sans-serif;
+	font-size: 60px;
+	left: 50%;
+	padding: 250px 0px 0px 0px;
+}
+
+.button1{
+	margin: auto;
+	display: block;
+	top: 20%;
+	background-color: #EE82EE;
+	color: black;
+	font-family: 'Common Pixel', sans-serif;
+	border: none;
+	padding: 20px 40px;
+	text-align: center;
+	font-size: 30px;
+	width: 350px;
+	
+}
+
+.button2{
+	margin: auto;
+	display: block;
+	top: 35%;
+	background-color: #EE82EE;
+	color: black;
+	font-family: 'Common Pixel', sans-serif;
+	border: none;
+	padding: 20px 40px;
+	text-align: center;
+	font-size: 30px;
+	width: 350px;
+}
+
+.button1:hover{
+	background-color: white;
+	color: black;
+}
+
+.button2:hover{
+	background-color: white;
+	color: black;
+}
+
+</style>
+	
+	</header>
+</body>
+</html>
+<?php
+?>
+
