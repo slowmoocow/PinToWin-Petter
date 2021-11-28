1fdbffbe67e84610dfef86e62bf8e65e8d0a59bd
Petter Tveit <78284025+slowmoocow@users.noreply.github.com>
2021-11-28 14:15:39 +0100

    Create saved.php

---
+++ code_snippets/saved.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>Points</title>
+</head>
+
+<body>
+	<header>
+		<h3>Score submitted!</h3>
+<div class="box">
+	<form action="config.php">
+		<input type="submit" value="Play again">
+	</form>
+</div>
+
+	
+<style media="screen">
+@import url('http://fonts.cdnfonts.com/css/common-pixel');
+
+body{
+	background-color:black;
+}
+
+header h3{
+	color: MediumSeaGreen;
+	text-align: center;
+	font-family: 'Common Pixel', sans-serif;
+	font-size: 30px;
+	left: 50%;
+	padding: 275px 0px 0px 0px;
+}
+
+.box input[type="submit"]{
+	margin: auto;
+	display: block;
+	top: 50%;
+	background-color: #EE82EE;
+	color: black;
+	font-family: 'Common Pixel', sans-serif;
+	border: none;
+	padding: 20px 40px;
+	text-align: center;
+	font-size: 30px;
+	width: 350px;
+}
+
+.box input[type="submit"]:hover{
+	background-color: white;
+	color: black;
+}
+
+</style>
+	</header>
+</body>
+</html>
